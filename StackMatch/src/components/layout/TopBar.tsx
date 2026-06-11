58089ed735827149e13ac41427a619269682d4f6
Sarah Grippa <sarah.grippa@dataciders.com>
2026-06-11 23:16:33 +0200

    feat: add logo.svg and integrate it into TopBar component

--- StackMatch/src/components/layout/TopBar.tsx
+++ StackMatch/src/components/layout/TopBar.tsx
@@ -1,12 +1,20 @@
 import { AppBar, AppBarSection, AppBarSpacer } from "@progress/kendo-react-layout";
+import logoSvg from "../../assets/logo.svg";
 
 export const TopBar = () => {
   return (
     <AppBar style={{ background: "var(--color-primary)", color: "#fff" }}>
       <AppBarSection>
-        <span style={{ fontSize: "1.25rem", fontWeight: 700, letterSpacing: "-0.5px" }}>
-          StackMatch
-        </span>
+        <div style={{ display: "flex", alignItems: "center", gap: "0.6rem" }}>
+          <img
+            src={logoSvg}
+            alt="StackMatch logo"
+            style={{ width: "28px", height: "28px", objectFit: "contain" }}
+          />
+          <span style={{ fontSize: "1.25rem", fontWeight: 700, letterSpacing: "-0.5px" }}>
+            StackMatch
+          </span>
+        </div>
       </AppBarSection>
       <AppBarSpacer />
     </AppBar>
